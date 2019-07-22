d490f17ca4122d89366f41973918a0a5e2f8ba9b
TheDG <dsinay@uc.cl>
2019-07-22 15:40:58 -0400

    Menu Basic Show

--- lunch_poll/urls.py
+++ lunch_poll/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('menu', views.menu, name='menu'),
     path('menu/new', views.menu_new, name='menu_new'),
+    path('menu/<int:menu_id>', views.menu_show, name='menu_show'),
 
 ]
--- lunch_poll/views/menu.py
+++ lunch_poll/views/menu.py
@@ -4,8 +4,9 @@ from django.contrib.auth.decorators import login_required, permission_required
 from lunch_poll.forms import MenuForm
 from lunch_poll.models import Menu
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.core.paginator import Paginator
 
 
 @login_required(login_url='/accounts/login/')
@@ -16,17 +17,27 @@ def menu(request):
         form = MenuForm(request.POST)
         if form.is_valid():
             form.save()
-            # TODO: redirect to show
+            # TODO: THink about redirect
             return HttpResponseRedirect(reverse('lunch_poll:index'))
         return render(request, 'lunch_poll/menu/new.html', {'form': form})
     # Index action
     else:
-        print("has")
-        menus = Menu.objects.all()
-        return render(request,'lunch_poll/menu/index.html', {'menus':menus})
+        menus = Menu.objects.all().order_by('menu_date')
+        paginator = Paginator(menus, 10)
+        page = request.GET.get('page')
+        menus = paginator.get_page(page)
+        return render(request, 'lunch_poll/menu/index.html', {'menus': menus})
+
 
 @login_required(login_url='/accounts/login/')
 @permission_required('lunch_poll.add_menu')
 def menu_new(request):
     form = MenuForm()
     return render(request, 'lunch_poll/menu/new.html', {'form': form})
+
+
+@login_required(login_url='/accounts/login/')
+@permission_required('lunch_poll.view_menu')
+def menu_show(request, menu_id):
+    menu = get_object_or_404(Menu, pk=menu_id)
+    return render(request, 'lunch_poll/menu/show.html', {'menu': menu})
